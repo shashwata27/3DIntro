2b49dbe2e7a995bffd5af147bd697da5a6d8538d
shashwata27 <sahas277@gmail.com>
2024-02-13 00:12:28 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Spline from '@splinetool/react-spline';
+
+function App() {
+  return (
+    <div className="App">
+       <Spline scene="https://prod.spline.design/L-Wcv3SIUN3BnO7D/scene.splinecode" />
+    </div>
+  );
+}
+
+export default App;
